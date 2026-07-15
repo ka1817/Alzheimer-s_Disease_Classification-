24d8be9dd378151979d6d54210e5652b6b8974da
PRANAV_REDDY <reddysaipranav6@gmail.com>
2026-07-15 11:09:45 +0530

    Updated Database

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@postgres:5432/alzheimer_db"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
